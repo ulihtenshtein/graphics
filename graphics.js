f4f04faa7b2d71fad9cb6bb968dec181cdd242ed
ulrih lihtenshtein <ulihtenshtein@gmail.com>
2013-09-14 00:27:24 +0600

    initial commit

---
+++ graphics.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("graph");
+var elem = document.getElementById("func-form");
+var func = document.getElementById("func");
+var string
+if( elem ) {
+	elem.addEventListener("click", onClick, false);
+}
+function onClick(e) {
+	var elem = e.target;
+	func.value +=
+}
